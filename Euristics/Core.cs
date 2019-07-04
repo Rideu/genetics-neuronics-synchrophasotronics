4c4a62fdad1af6c1f333d6320cab9f0d21b1218c
Rideu <relso@bk.ru>
2019-07-04 13:46:37 +0300

    initial

---
+++ Euristics/Core.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using WinD = System.Drawing;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.ComponentModel;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Euristics
+{
+
+
+
+    public class Core : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        static Random rand = new Random();
+
+        public static float Rand => (float)rand.NextDouble();
+
+
+        public static Viewport PrimaryViewport;
+
+        public Core()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            Simplex.Init(GraphicsDevice);
+            TargetElapsedTime = new TimeSpan(11110);
+            base.Initialize();
+        }
+
+
+
+        Timer t = new Timer(100);
+        Timer successpopup = new Timer(600);
+
+        public static SpriteFont font;
+        int totalgens;
+        int successgens;
+
+        protected override void LoadContent()
+        {
+            font = Content.Load<SpriteFont>("fonts//Xolonium");
+            font.DefaultCharacter = ' ';
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Generate();
+
+            t.OnFinish += () =>
+            {
+                Genetics.Evolve(generation, () => Rand * Rand * Rand * Rand);
+                //t.Reset(false);
+                t.Start();
+            };
+
+            t.Start();
+        }
+
+        List<ResultEntry> results = new List<ResultEntry>();
+        ResultEntry last;
+        Generation generation;
+
+        public static int MaxGenes = 1000;
+
+        void Generate()
+        {
+            if (generation != null && generation.genes.Count > MaxGenes)
+            {
+                results.Add(new ResultEntry(generation));
+                if (!successpopup.IsRunning)
+                {
+                    successpopup.Reset(false);
+                    successpopup.Start();
+                    last = results[results.Count-1];
+                }
+                successgens++;
+            }
+            List<Gene> genes = new List<Gene>();
+            for (int i = 0; i < 20; i++)
+            {
+                // [Energy : ResAmount : Speed] => Result
+                genes.Add(new Gene(Rand * 20, Rand * 100, Rand * 100, 0));
+            }
+            generation = new Generation(genes);
+            totalgens++;
+        }
+
+
+
+
+
+
+
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            PrimaryViewport = GraphicsDevice.Viewport;
+            Control.Update();
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            t.Update(gameTime.ElapsedGameTime.Milliseconds);
+            successpopup.Update(gameTime.ElapsedGameTime.Milliseconds);
+            if (generation.CanRestart)
+                Generate();
+            base.Update(gameTime);
+        }
+
+
+
+
+
+
+
+
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(new Color(40, 40, 40));
+
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+            {
+                //successgens / totalgens
+                spriteBatch.DrawFill(new Rectangle(0, 0, 225, 800), Color.Gray);
+                if (totalgens > 0)
+                {
+                    spriteBatch.DrawString(font, $"Total   -   {totalgens} \nSuccess ({successgens})  -   {(1f * successgens / totalgens):0.000}", new Vector2(245, 10), Color.White);
+                    int i = 0, j = 0;
+                    ResultEntry hov = new ResultEntry();
+                    foreach (var r in results)
+                    {
+                        if (i > 36)
+                        {
+                            j += 1;
+                            i = 0;
+                        }
+                        var rc = new Rectangle(1 + 6 * i, 1 + 6 * j, 5, 5);
+                        if (rc.Contains(Control.MousePos))
+                        {
+                            hov = r;
+                        }
+
+                        spriteBatch.DrawFill(rc, new Color(r.data[0] / 100, r.data[1] / 100, r.data[2] / 100));
+                        i++;
+                    }
+                    float x = successpopup.Progress;
+                    float y = (float)(-(x - 1) * (x + 1) / 0.5 / 2);
+                    if (!last.Equals(default(ResultEntry)))
+                        spriteBatch.DrawString(font, $"+S[{last.data[0]:0.00}   |   {last.data[1]:0.00}   |   {last.data[2]:0.00}]", new Vector2(15, 10 + 30 * y), last.col * (1 - y));
+
+                    if (!hov.Equals(default(ResultEntry)))
+                    {
+                        Rectangle dr;
+                        if (PrimaryViewport.Height - Control.MousePos.Y < 100)
+                            dr = new Rectangle(Control.MousePos.ToPoint() - new Point(100, 100), new Point(100, 100));
+                        else dr = new Rectangle(Control.MousePos.ToPoint(), new Point(100, 100));
+                        spriteBatch.DrawFill(dr, new Color(hov.data[0] / 100, hov.data[1] / 100, hov.data[2] / 100));
+                        spriteBatch.DrawString(font, $"E : {(hov.data[0]):0.000}\nR:{(hov.data[1]):0.000}\nS:{(hov.data[2]):0.000}", Control.MousePos + new Vector2(20), Color.White);
+                    }
+                }
+            }
+            spriteBatch.End();
+            generation.Draw(spriteBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Euristics/Genetics.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using WinD = System.Drawing;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.ComponentModel;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+
+using sRectangle = Microsoft.Xna.Framework.Rectangle;
+using static System.Math;
+using static Euristics.Core;
+
+namespace Euristics
+{
+    public class Gene
+    {
+        internal float[] keyvalues;
+        public float[] GetKeyvalues => keyvalues;
+
+        public Gene(params float[] genecode)
+        {
+            keyvalues = genecode;
+        }
+
+        public void Update(Action a)
+        {
+            a.Invoke();
+        }
+    }
+
+    public struct ResultEntry
+    {
+        public float[] data;
+        public int gens;
+        public Color col;
+        public ResultEntry(Generation g)
+        {
+            gens = g.gen;
+            data = g.genes[0].keyvalues;
+            col = new Color(data[0] / 100, data[1] / 100, data[2] / 100);
+        }
+    }
+
+    public class Generation
+    {
+        internal List<Gene> genes;
+
+        public Generation(List<Gene> genes)
+        {
+            this.genes = genes;
+        }
+
+        internal int gen;
+        public int Step => gen;
+
+        bool restart;
+        public bool CanRestart { get => restart; internal set => restart = value; }
+
+        public void Draw(SpriteBatch batch)
+        {
+            batch.Begin(SpriteSortMode.Deferred);
+            {
+                batch.DrawString(font, $"Generation: {gen}", new Vector2(245, 70), Color.White);
+                int i = 0, j = 0;
+                foreach (var g in genes)
+                {
+                    if (i > 20)
+                    {
+                        j += 1;
+                        i = 0;
+                    }
+                    batch.DrawFill(new Rectangle(245 + 25 * i, 100 + 30 * j, 20, 20), new Color(g.keyvalues[0] / 100, g.keyvalues[1] / 100, g.keyvalues[2] / 100));
+                    i++;
+                }
+            }
+            batch.End();
+        }
+    }
+
+    public static class Genetics
+    {
+        public static Gene SPC(Gene parent1, Gene parent2)
+        {
+            float[] crossbuffer1 = parent1.GetKeyvalues;
+            float[] crossbuffer2 = parent2.GetKeyvalues;
+
+            for (int i = 0; i < crossbuffer1.Length; i++)
+            {
+                float sp = Rand;
+
+                crossbuffer1[i] = crossbuffer1[i] * (sp + 0.05f) + crossbuffer2[i] * (1 - (sp + 0.05f));
+            }
+
+            return new Gene(crossbuffer2);
+        }
+
+        public static void Evolve(Generation pop, Func<float> f)
+        {
+            if (pop.genes.Count > MaxGenes || pop.genes.Count == 0)
+            {
+                pop.CanRestart = true;
+                return;
+            }
+
+            foreach (var g in pop.genes)
+            {
+                g.Update(() => { g.keyvalues[3] = g.keyvalues[0] / g.keyvalues[2] / g.keyvalues[1]; });
+            }
+
+            var pre = pop.genes.FindAll(n =>
+            {
+                // Volatility
+                float res = Rand * n.keyvalues[3];
+                return res > f();
+            });
+
+            pop.genes = pre;
+            var gc = pop.genes.Count;
+            for (int i = 0; i < gc; i++)
+            {
+
+                pop.genes.Add(SPC(pre[(int)(Rand * gc)], pre[(int)(Rand * gc)]));
+            }
+            pop.gen++;
+        }
+
+        public static void Evolve(List<Gene> pop, params float[] cond)
+        {
+            //pop.Find(n => 
+            //{
+            //    float res = 0;
+            //    for (int i = 0; i < cond.Length; i++)
+            //    {
+            //        res += n.keyvalues[i] > cond[i] ? n.keyvalues[i] : n.keyvalues[i] * 0.5f;
+            //    }
+            //    return 
+            //});
+        }
+    }
+}
